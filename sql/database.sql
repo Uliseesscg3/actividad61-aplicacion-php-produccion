f14005c70cce4377a4aea769a446adeb8835d8ac
Ubuntu <ubuntu@ip-192-168-33-4.ec2.internal>
2025-03-07 12:43:47 +0000

    modificacion .env y sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,15 +1,19 @@
-CREATE TABLE empleados (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  apellido VARCHAR(100) NOT NULL,
+CREATE TABLE pokemon (
+  id INT UNSIGNED PRIMARY KEY,
   nombre VARCHAR(100) NOT NULL,
-  edad INT UNSIGNED NOT NULL,
-  puesto VARCHAR(100) NOT NULL
+  tipo1 VARCHAR(100) NOT NULL,
+  tipo2 varchar(100),
+  habilidad varchar(100) NOT NULL,
+  stat_total INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO empleados (apellido, nombre, edad, puesto) VALUES('Coloma', 'Javier', 25, 'Contable');
-INSERT INTO empleados (apellido, nombre, edad, puesto) VALUES('Oviedo', 'Carmen', 34, 'Administrativo');
-INSERT INTO empleados (apellido, nombre, edad, puesto) VALUES('Vargas', 'Pascual', 19, 'Dependiente');
-INSERT INTO empleados (apellido, nombre, edad, puesto) VALUES('Donoso', 'Maria', 45, 'Gerente');
-INSERT INTO empleados (apellido, nombre, edad, puesto) VALUES('Celis', 'Manuel', 56, 'Dependiente');
-INSERT INTO empleados (apellido, nombre, edad, puesto) VALUES('Palencia', 'Jana', 31, 'Dependiente');
-INSERT INTO empleados (apellido, nombre, edad, puesto) VALUES('Zamanillo', 'Pedro', 63, 'Repartidor');
+INSERT INTO pokemon VALUES('25', 'Pikachu', "Electrico", '',"Electricidad Estatica", 320);
+INSERT INTO pokemon VALUES('150', 'Mewtwo', "Psiquico", '', "Presion", 680);
+INSERT INTO pokemon VALUES('763', 'Tsareena', "Planta", '', "Defensa Hoja", 510);
+INSERT INTO pokemon VALUES('233', 'Porygon2', "Normal", '', "Descarga", 515);
+INSERT INTO pokemon VALUES('111', 'Rhyhorn', "Tierra", 'Roca', "Cabeza Roca", 345);
+INSERT INTO pokemon VALUES('984', 'Colmilargo', "Tierra", 'Lucha', "Paleosintesis", 570);
+INSERT INTO pokemon VALUES('823', 'Corviknight', "Volador", 'Acero', "Coraza Reflejo", 495);
+INSERT INTO pokemon VALUES('333', 'Swablu', "Volador", 'Normal', "Cura Natural", 310);
+INSERT INTO pokemon VALUES('999', 'Gimmighoul ', "Fantasma", '', "Fuga", 300);
+INSERT INTO pokemon VALUES('1025', 'Pecharunt', "Veneno", 'Fantasma', "Titere Toxico", 600);
